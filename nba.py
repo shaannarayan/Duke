a356dfde3e3abf1e84788c299a1a924717b13b5b
shaannarayan <shaan@quantworks.com>
2020-07-01 11:22:51 -0400

    sorted dataset

--- nba.py
+++ nba.py
@@ -7,10 +7,10 @@ nba = pd.read_csv("Duke.csv")
 #Created ratio column
 nba['Rebounds/Fouls'] = nba.apply(lambda row: row['TRB']/row['PF'] if row['PF'] != 0 else 0, axis=1)
 
-#Created new df with only relevent data
+#Created new df with only relevent data(sorted)
 df = pd.DataFrame() 
 df['Player_Name'] = nba['Player'] 
 df['Rebounds/Fouls'] = nba['Rebounds/Fouls']
-print(type(df))
-df.to_csv("Duke_2019-2020.csv")
+new_df = df.sort_values('Rebounds/Fouls',ascending = False)
+new_df.to_csv("Duke_2019-2020.csv")
 
